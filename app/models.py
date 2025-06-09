fc25a27a0d62a878e60a4cb9e1397a7b5ad93b23
Andrew_Karev <karev.py@gmail.com>
2025-06-09 22:21:43 +0300

    Add bootstrap

--- app/models.py
+++ app/models.py
@@ -2,10 +2,12 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db, login
+from app import db, login, app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
+import jwt
 
 @login.user_loader
 def load_user(id):
@@ -24,6 +26,19 @@ followers = sa.Table(
 
 class User(UserMixin, db.Model):
 
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
