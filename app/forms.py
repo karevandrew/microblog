3b04c4873ba82aee874e555efaaf1dc7e7d5aeaf
Andrew_Karev <karev.py@gmail.com>
2025-06-10 15:51:58 +0300

    PEP 8 edits

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,18 @@
+"""Manage app forms structure."""
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField,\
+    BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError,\
+    DataRequired, Email, EqualTo, Length
 import sqlalchemy as sa
 from app import db
 from app.models import User
 
+
 class LoginForm(FlaskForm):
+    """Define login form structure."""
+
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
@@ -13,6 +20,8 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+    """Define registration form structure."""
+
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -21,12 +30,14 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
+        """Check if username is correct."""
         user = db.session.scalar(sa.select(User).where(
             User.username == username.data))
         if user is not None:
             raise ValidationError('Please use a different username.')
 
     def validate_email(self, email):
+        """Check if email is correct."""
         user = db.session.scalar(sa.select(User).where(
             User.email == email.data))
         if user is not None:
@@ -34,15 +45,19 @@ class RegistrationForm(FlaskForm):
 
 
 class EditProfileForm(FlaskForm):
+    """Define edit profile form structure."""
+
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
     def __init__(self, original_username, *args, **kwargs):
+        """Save original username in memory."""
         super().__init__(*args, **kwargs)
         self.original_username = original_username
 
     def validate_username(self, username):
+        """Check if username is correct."""
         if username.data != self.original_username:
             user = db.session.scalar(sa.select(User).where(
                 User.username == username.data))
@@ -51,19 +66,14 @@ class EditProfileForm(FlaskForm):
 
 
 class EmptyForm(FlaskForm):
+    """Define form with only button."""
+
     submit = SubmitField('Submit')
 
+
 class PostForm(FlaskForm):
+    """Define post form."""
+
     post = TextAreaField('Say something', validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
-
-class ResetPasswordRequestForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
-
-class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Request Password Reset')
